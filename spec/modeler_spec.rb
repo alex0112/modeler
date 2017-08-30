67478deaa6583d7d54403a620d828dd43c594660
alex0112 <alarsen0112@gmail.com>
2017-08-29 23:01:49 -0600

    Renamed modeler to scraper

--- spec/modeler_spec.rb
+++ spec/modeler_spec.rb
@@ -1,19 +1,21 @@
-require_relative '../modeler.rb'
+# require_relative '../modeler.rb'
 
-describe 'Modeler' do
-  before :all do
-    @modeler = Modeler.new(uri: 'https://www.google.com', out_file: 'google_home_page_class.rb')
-  end
+## Todo:  Build page object modeler using the scraper class for use in Selenium tests.
 
-  context 'google.com' do
-    it 'should generate a class method called "navigate" for www.google.com' do
-    end
-    it 'should generate a *getter* for the search input on www.google.com' do
-    end
-    it 'should generate a *setter* for the search input on www.google.com' do
-    end
-    it 'should generate a *getter* for the search button on www.google.com' do
-    end
-  end
-end
+# describe 'Modeler' do
+#   before :all do
+#     @modeler = Modeler.new(uri: 'https://www.google.com', out_file: 'google_home_page_class.rb')
+#   end
+
+#   context 'google.com' do
+#     it 'should generate a class method called "navigate" for www.google.com' do
+#     end
+#     it 'should generate a *getter* for the search input on www.google.com' do
+#     end
+#     it 'should generate a *setter* for the search input on www.google.com' do
+#     end
+#     it 'should generate a *getter* for the search button on www.google.com' do
+#     end
+#   end
+# end
 
